7e9d56897134f017846960b9146861021eb27616
vladic14 <136345362+vladic14@users.noreply.github.com>
2023-12-19 19:39:01 +0200

    Add files via upload

---
+++ Problematest/Problematest/Controllers/TodoController.cs
@@ -0,0 +1,73 @@
+﻿namespace Problematest.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Problematest.Models;
+    using System;
+    using System.Collections.Generic;
+
+    public class TodoController : Controller
+    {
+        private static List<TodoItem> listaSarcini = new List<TodoItem>();
+
+        public IActionResult Index()
+        {
+            return View(listaSarcini);
+        }
+
+        public IActionResult Adauga()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adauga(TodoItem sarcina)
+        {
+            sarcina.Id = listaSarcini.Count + 1;
+            listaSarcini.Add(sarcina);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Editeaza(int id)
+        {
+            var sarcina = listaSarcini.Find(item => item.Id == id);
+            if (sarcina == null)
+            {
+                return NotFound();
+            }
+            return View(sarcina);
+        }
+
+        [HttpPost]
+        public IActionResult Editeaza(TodoItem sarcina)
+        {
+            var existent = listaSarcini.FindIndex(item => item.Id == sarcina.Id);
+            if (existent != -1)
+            {
+                listaSarcini[existent] = sarcina;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Sterge(int id)
+        {
+            var sarcina = listaSarcini.Find(item => item.Id == id);
+            if (sarcina == null)
+            {
+                return NotFound();
+            }
+            return View(sarcina);
+        }
+
+        [HttpPost]
+        public IActionResult StergeConfirmare(int id)
+        {
+            var sarcina = listaSarcini.Find(item => item.Id == id);
+            if (sarcina != null)
+            {
+                listaSarcini.Remove(sarcina);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ Problematest/Problematest/Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Problematest.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Nume { get; set; }
+        public DateTime DataLimita { get; set; }
+        public StatusSarcina Status { get; set; }
+    }
+
+    public enum StatusSarcina
+    {
+        Pending = 0,
+        Done = 1,
+        Rejected = 2
+    }
+
+}
